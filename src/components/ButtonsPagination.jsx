8e2dc11b8284f38dae578c66224efe62c7d96278
Jesus Emmanuel De Leon Lerma <jesuspin2626@gmail.com>
2022-07-31 17:16:54 -0500

    Maquetacion grid y cards de pokemons

--- src/components/ButtonsPagination.jsx
+++ src/components/ButtonsPagination.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import iconoRight from "../assets/images/der.png";
+import iconoLeft from "../assets/images/iz.png";
+
 
 export const ButtonsPagination = () => {
-  return (
-    <div>ButtonsPagination</div>
-  )
+    return (
+        <>
+            <div className="group-buttons">
+                <button className='btn-pagination'> <img className='icono-izq' src={iconoLeft} alt="" /> Atras</button>
+                <button className='btn-pagination'>Siguiente <img className='icono-der' src={iconoRight} alt="" /> </button>
+            </div>
+        </>
+    )
 }
--- src/components/CardPokemon.jsx
+++ src/components/CardPokemon.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
 export const CardPokemon = () => {
-  return (
-    <>
-    </>
-  )
+    return (
+        <>
+            <div   className="card-colum">
+                <div className="card-pokemon">
+                    <img className='card-img-pokemon'  alt="" />
+                    <h3 className=''>#1</h3>
+                    <h3 className='title-card-pokemon'>pikachu</h3>
+                </div>
+            </div>
+        </>
+    )
 }
