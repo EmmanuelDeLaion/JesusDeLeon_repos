791c9eb037064fae6f204157f0c6190dfd820f03
Jesus Emmanuel De Leon Lerma <jesuspin2626@gmail.com>
2022-07-31 16:42:33 -0500

    Inicializacion de maquetacion y assets

--- src/PokedexApp.jsx
+++ src/PokedexApp.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
+import { CardViewPokemon } from './components/CardViewPokemon'
+import { GridCardPokemons } from './components/GridCardPokemons'
+import { Navbar } from './components/Navbar'
 import { SearchPokemon } from './components/SearchPokemon'
 import { TitleApp } from './components/TitleApp'
 
 export const PokedexApp = () => {
   return (
     <>
-        <TitleApp></TitleApp>
-        <SearchPokemon></SearchPokemon>
+        <Navbar></Navbar>
+        
+        <TitleApp></TitleApp> 
+
+        <div className="container-search">
+            <div className="input-search">
+                <div className="search">
+                    <SearchPokemon></SearchPokemon>
+                </div>
+            </div>
+        </div>
+
+        <div className="grid-layout">
+            <div className="grid-card">
+                <GridCardPokemons></GridCardPokemons>
+            </div>
+
+            <div className="grid-card">
+                <CardViewPokemon></CardViewPokemon>
+            </div>
+        </div>
+
     </>
   )
 }
